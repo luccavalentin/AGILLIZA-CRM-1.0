5a3178f5793537b682e668ad4bf73b634f45c399
luccavalentin <luccasantana88@gmail.com>
2026-09-19 15:31:17 -0300

    Acessibilidade: busca global e menu mobile com título/descrição para leitor de tela
    O Radix acusava DialogContent sem DialogTitle/Description no console a cada
    abertura (QA 19/09). Título e descrição visualmente ocultos (sr-only).

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -6,7 +6,7 @@ import { Command as CommandPrimitive } from "cmdk";
 import { Search } from "lucide-react";
 
 import { cn } from "@/lib/utils";
-import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogDescription, DialogTitle } from "@/components/ui/dialog";
 
 const Command = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive>,
@@ -31,6 +31,12 @@ const CommandDialog = ({
   return (
     <Dialog {...props}>
       <DialogContent className="overflow-hidden p-0">
+        {/* Título e descrição só para leitores de tela: sem eles o Radix acusa
+            a janela como inacessível (erro no console a cada abertura). */}
+        <DialogTitle className="sr-only">Buscar no sistema</DialogTitle>
+        <DialogDescription className="sr-only">
+          Digite para buscar clientes, simulações, propostas e tarefas.
+        </DialogDescription>
         <Command
           shouldFilter={shouldFilter}
           className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5"
